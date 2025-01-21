0159b0d45d46feb794746f1cddfd8376cffa12b2
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2025-01-21 14:03:50 -0800

    Db schema modified, recreated it

--- api/api.py
+++ api/api.py
@@ -1,6 +1,7 @@
 import time
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import click
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
@@ -10,7 +11,8 @@ class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     ingredients = db.Column(db.String(500), nullable=False)
-    description = db.Column(db.Text, nullable=False)
+    instructions = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=True, default='Delicious. You need to try it!')
     image_url = db.Column(db.String(500), nullable=True, default="https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1") 
     servings = db.Column(db.Integer, nullable=False)
     def __repr__(self):
